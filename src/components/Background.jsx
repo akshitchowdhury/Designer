d0399e00811dc86a6e2431020748e99312e5a990
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-19 20:33:37 +0530

    d1

---
+++ src/components/Background.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import bkgd from '../assets/bkgd.jpeg'
+const Background = () => {
+  return (
+    <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        pointerEvents: 'none',
+        backgroundImage: `url(${bkgd})`,
+        backgroundRepeat: 'repeat',
+        backgroundSize: 'cover',
+        opacity: 0.5, // Adjust the opacity as needed
+      }}>
+      
+    </div>
+  )
+}
+
+export default Background
