a79e14b627621ba402da93ee997d9ba3734297f2
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-20 21:18:23 +0530

    r2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Background from './components/Bakground/Background.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Home from './components/Home/Home.jsx';
+import Contacts from './components/Contacts/Contacts.jsx';
 
 function App() {
   return (
     <div className="App">
 
     <Home></Home>
+    <Contacts/>
+
     <Navbar/>
     <Background/>      
     </div>
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contacts = () => {
+  return (
+    <div>
+        <p>
+            <h1>This is the Contacts page</h1>
+        </p>      
+    </div>
+  )
+}
+
+export default Contacts
