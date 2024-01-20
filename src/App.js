b742dff884f11bf27e5699c2c0e65e374e8ead1a
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-20 21:26:36 +0530

    r3

--- src/App.js
+++ src/App.js
@@ -4,17 +4,39 @@ import Background from './components/Bakground/Background.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Home from './components/Home/Home.jsx';
 import Contacts from './components/Contacts/Contacts.jsx';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
 
     <Home></Home>
-    <Contacts/>
-
+    <Link to="/contacts">
+    Contacts
+    </Link>
     <Navbar/>
     <Background/>      
+    
+    <Switch>
+
+      <Route path='/contacts'>
+      <Contacts/>  
+      </Route>
+    </Switch>
+    
+    
+    
     </div>
+
+
+
+    </Router>
   );
 }
 
