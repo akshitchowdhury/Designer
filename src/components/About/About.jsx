b542eb62ff4565cecb69ede5ab407f90a493d541
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-27 22:57:40 +0530

    s5

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 const About = () => {
   return (
-
-    <div>This is the Web page of DevAshura </div>
+    <div className='base'>
+    <div className='contacts'>This is the Web page of DevAshura </div>
+    </div>
   )
 }
 
