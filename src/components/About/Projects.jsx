cc0d0e93633791cea29cbe5484fad332b5106cc4
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-28 04:34:18 +0530

    s8

--- src/components/About/Projects.jsx
+++ src/components/About/Projects.jsx
@@ -3,13 +3,24 @@ import './Projects.css'
 const Projects = () => {
     const data = {name : "DevAsura",
 topic : "Projects",
-statement : "These are my"}
+statement : "These are my",
+check : true}
   return (
 <>
 <div className='base'>
+if(check===true){
+
+
 <div className='projects'>
     {data.statement} {data.topic}
 </div>
+}
+
+else{
+    <div className='projects'>
+    Nothing here
+</div>
+}
 </div>
 </>
   )
