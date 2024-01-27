dc2244ad928d05daea455a5a4a5d6a96d64c7e06
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-01-27 23:15:27 +0530

    s2

---
+++ src/components/About/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+<>
+<div className='base'>
+<p className='projects'>
+    List of my Projects
+</p>
+</div>
+</>
+  )
+}
+
+export default Projects
